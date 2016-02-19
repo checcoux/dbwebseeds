6c62a8f013dc57cfd13de5414d5f396a32b83f7c
Andrea Ros <andrea@donboscoland.it>
2016-02-19 16:43:28 +0100

    test del modello section

--- test/models/section_test.rb
+++ test/models/section_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "gli attributi della sezione non devono essere vuoti" do
+    section = Section.new
+    assert section.invalid?
+    assert section.errors[:titolo].any?
+    assert section.errors[:descrizione].any?
+  end
 end

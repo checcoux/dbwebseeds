eb4bee4a205c7d62df425166233584a6923be2d9
Andrea Ros <andrea@donboscoland.it>
2016-05-14 18:04:05 +0200

    preparazione per ambiente di produzione

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Dbwebcore
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += Ckeditor.assets
   end
 end

865efbd4d9054fed8c278d4cb5d95ccc25c1f3f0
Andrea Ros <andrea@donboscoland.it>
2016-05-22 08:37:45 +0200

    sistemate inserisci_colonna_prima e _dopo

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -143,7 +143,7 @@ class ColumnsController < ApplicationController
     end
   end
 
-  def inserisci_colonna_prima
+  def inserisci_colonna_prima1
     @row = @column.row
     posizione = @column.ordine
 
@@ -175,12 +175,49 @@ class ColumnsController < ApplicationController
     @row = @column.row
     posizione = @column.ordine
 
-    # procedo solo se la differenza tra la somma delle larghezze delle colonne precedenti e il loro numero è maggiore di zero
-    columns = Column.where('ordine <= ? AND row_id = ?', posizione, @row.id).order(ordine: :asc)
+    # procedo solo se la differenza tra la somma delle larghezze delle colonne successive e il loro numero è maggiore di zero
+    columns = Column.where('ordine > ? AND row_id = ?', posizione, @row.id).order(ordine: :asc)
 
     if columns.sum("larghezza") > columns.count  then
       # aumento di uno l'ordine delle colonne con ordine > posizione
-      successive = Column.where('ordine > ? AND row_id = ?', posizione, @row.id).order(ordine: :asc)
+
+      resto = 0
+      columns.each do |colonna|
+        if colonna.larghezza > 1 then
+          resto = resto + 1
+          colonna.larghezza = colonna.larghezza - 1
+        end
+
+        colonna.ordine = colonna.ordine + 1
+        colonna.save
+      end
+      if resto >= 1 then # dovrebbe sempre essere così perché il controllo è stato fatto a monte
+        # creo una colonna vuota
+        @column2 = Column.create(ordine: posizione + 1, larghezza: resto, row_id: @row.id, contenuto: '<p>Cantami o Diva del pelide Achille l\'ira funesta...</p>')
+        @column2.save
+      end
+    else
+      # vedo se posso stringere un po' la colonna selezionata
+      if @column.larghezza > 1 then
+        @column.larghezza = @column.larghezza - 1
+        @column.save
+
+        @column2 = Column.create(ordine: posizione + 1, larghezza: 1, row_id: @row.id, contenuto: '<p>Cantami o Diva del pelide Achille l\'ira funesta...</p>')
+        @column2.save
+      end
+    end
+  end
+
+  def inserisci_colonna_prima
+    @row = @column.row
+    posizione = @column.ordine
+
+    # procedo solo se la differenza tra la somma delle larghezze delle colonne precedenti e il loro numero è maggiore di zero
+    columns = Column.where('ordine < ? AND row_id = ?', posizione, @row.id).order(ordine: :asc)
+    successive = Column.where('ordine >= ? AND row_id = ?', posizione, @row.id).order(ordine: :asc)
+
+    if columns.sum("larghezza") > columns.count  then
+      # aumento di uno l'ordine delle colonne con ordine >= posizione
       successive.each do |colonna|
         colonna.ordine = colonna.ordine + 1
         colonna.save
@@ -196,7 +233,20 @@ class ColumnsController < ApplicationController
       end
       if resto >= 1 then # dovrebbe sempre essere così perché il controllo è stato fatto a monte
         # creo una colonna vuota
-        @column2 = Column.create(ordine: posizione + 1, larghezza: resto, row_id: @row.id, contenuto: '<p>Cantami o Diva del pelide Achille l\'ira funesta...</p>')
+        @column2 = Column.create(ordine: posizione, larghezza: resto, row_id: @row.id, contenuto: '<p>Cantami o Diva del pelide Achille l\'ira funesta...</p>')
+        @column2.save
+      end
+    else
+      # vedo se posso stringere un po' la colonna selezionata
+      if @column.larghezza > 1 then
+        @column.larghezza = @column.larghezza - 1
+        @column.save
+        successive.each do |colonna|
+          colonna.ordine = colonna.ordine + 1
+          colonna.save
+        end
+
+        @column2 = Column.create(ordine: posizione, larghezza: 1, row_id: @row.id, contenuto: '<p>Cantami o Diva del pelide Achille l\'ira funesta...</p>')
         @column2.save
       end
     end

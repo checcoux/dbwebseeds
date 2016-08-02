0c6427005a644ed543ed15e61a61795739cb516d
Andrea Ros <andrea@donboscoland.it>
2016-08-02 16:22:46 +0200

    applicazione di header e footer a tutte le pagine

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,6 +47,7 @@ class PagesController < ApplicationController
   # GET /pages/1
   # GET /pages/1.json
   def show
+
   end
 
   # GET /pages/new
--- app/models/page.rb
+++ app/models/page.rb
@@ -11,4 +11,32 @@ class Page < ActiveRecord::Base
         [ section.percorso, :titolo ]
     ]
   end
+
+  def trova_header
+    # cerchiamo un header
+    # esiste un header per questa sezione?
+    intestazione = Page.find_by section: section, header: true
+
+    # se non esiste cerco un header nella sezione principale
+    if !intestazione
+      sezione = Section.find_by principale: true
+      intestazione = Page.find_by section: sezione, header: true
+    end
+
+    intestazione
+  end
+
+  def trova_footer
+    # cerchiamo un footer
+    # esiste un footer per questa sezione?
+    piedipagina = Page.find_by section: section, footer: true
+
+    # se non esiste cerco un footer nella sezione principale
+    if !piedipagina
+      sezione = Section.find_by principale: true
+      piedipagina = Page.find_by section: sezione, footer: true
+    end
+
+    piedipagina
+  end
 end

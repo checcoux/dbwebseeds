eca5a0939e2d9480485615a2ddc7931ef39594ae
Andrea Ros <andrea@donboscoland.it>
2016-08-27 17:35:20 +0200

    modifiche al file seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-section = Section.create(titolo: 'HOME', descrizione: 'Sezione principale del sito.')
+# viene creata una sezione principale di default
+section = Section.create(titolo: 'Principale', descrizione: 'Sezione principale del sito.', principale: true, percorso: '')
 
-page = Page.create(titolo: 'Home page', section_id: section.id)
+# viene creata una home page di default per la sezione principale
+page = Page.create(titolo: 'Home page', section_id: section.id, home: true, slug: 'home-page', visibile: true)
 
 row = Row.create(ordine: 1, page_id: page.id, estesa: false, colore_sfondo: '')
 
-column = Column.create(ordine: 1, contenuto: 'Cantami o diva del pelide Achille l\'ira funesta...', row_id: row.id, larghezza: 12)
+Column.create(ordine: 1, contenuto: 'Cantami o diva del pelide Achille l\'ira funesta...', row_id: row.id, larghezza: 12)
+
+# viene creato un amministratore di default
+user = User.create(email: 'admin', password: '123456', password_confirmation: '123456', admin: true)

bee0984406edff9af50529f05e28f9ecaf8f4883
Andrea Ros <andrea@donboscoland.it>
2016-10-02 10:41:58 +0200

    stili dei bottoni

--- public/js/ckeditor_my_styles.js
+++ public/js/ckeditor_my_styles.js
@@ -2,8 +2,24 @@
 // gli stili dovranno avere una corrispondenza nei css del sito
 
 CKEDITOR.stylesSet.add( 'my_styles', [
+    // Object-level styles
+    { name: 'Menu', element: 'ul', attributes: { 'class': 'menu' } },
+    { name: 'Bottone (primary)', element: 'a', attributes: { 'class': 'button' } },
+    { name: 'Bottone (secondary)', element: 'a', attributes: { 'class': 'secondary button' } },
+    { name: 'Bottone (success)', element: 'a', attributes: { 'class': 'success button' } },
+    { name: 'Bottone (warning)', element: 'a', attributes: { 'class': 'warning button' } },
+    { name: 'Bottone (alert)', element: 'a', attributes: { 'class': 'alert button' } },
+    { name: 'Bottone (disabled)', element: 'a', attributes: { 'class': 'disabled button' } },
+    
+    { name: 'Bottone vuoto (primary)', element: 'a', attributes: { 'class': 'hollow button' } },
+    { name: 'Bottone vuoto (secondary)', element: 'a', attributes: { 'class': 'hollow secondary button' } },
+    { name: 'Bottone vuoto (success)', element: 'a', attributes: { 'class': 'hollow success button' } },
+    { name: 'Bottone vuoto (warning)', element: 'a', attributes: { 'class': 'hollow warning button' } },
+    { name: 'Bottone vuoto (alert)', element: 'a', attributes: { 'class': 'hollow alert button' } },
+    { name: 'Bottone vuoto (disabled)', element: 'a', attributes: { 'class': 'hollow disabled button' } },
+
     // Block-level styles
-    { name: 'Blue Title', element: 'h2', attributes: { 'class': 'bleu' } },
+    { name: 'Bleue Title', element: 'h2', attributes: { 'class': 'bleu' } },
     { name: 'Red Title' , element: 'h3', attributes: { 'class': 'rouge' } },
 
     // Inline styles

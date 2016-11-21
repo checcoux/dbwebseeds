fa5111058348a4aa1a920c21fe1795d77df05662
Andrea Ros <andrea@donboscoland.it>
2016-11-21 18:19:08 +0100

    task importazione articoli

--- lib/tasks/importer/importer.rb
+++ lib/tasks/importer/importer.rb
@@ -1,5 +1,3 @@
-require File.dirname(__FILE__) +  "/articoli_importer"
-
 class Importer
 
   # include sub-modules
@@ -38,6 +36,30 @@ class Importer
   # use old database (= switch to old database)
   def use_old_database
     ActiveRecord::Base.connection.execute("use #{@oldDb}")
+  end
+
+  # import articoli
+  def import_articoli
+    puts "Importing articles..."
+    use_new_database
+    articoli = ActiveRecord::Base.connection.execute('
+      SELECT * from articoli LIMIT 10 order by id
+      ')
+
+    for i in 0...articoli.count do
+      row = articoli.get_row i
+
+      # user = User.new(first_name: row.get("firstname"),
+      # last_name: row.get("lastname"),
+      # email: row.get("mail"),
+      # isfemale: row.get("isfemale"))
+      begin
+        # user.save!
+        puts "Importazione articolo #{row.get("id")} #{row.get("titolo")}"
+      rescue Exception => e
+        # puts "Failed to save #{row.get("firstname")} #{row.get("lastname"): #{e.message}"
+      end
+    end
   end
 end
 

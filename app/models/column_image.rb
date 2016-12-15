d29c624a72c3d82499fcb6de971ec505ee6121a0
Andrea Ros <andrea@donboscoland.it>
2016-12-15 19:04:24 +0100

    immagini in vari formati

--- app/models/column_image.rb
+++ app/models/column_image.rb
@@ -5,9 +5,22 @@ class ColumnImage < ActiveRecord::Base
                     :hash_secret => "hj42ZZ5!76",
                     :url  => "/img/column/:hash.:extension",
                     :path => ":rails_root/public/img/column/:hash.:extension",
-                    :styles => { :content => '1800>', :thumb => '400x250#' },
+                    :styles => {
+                        :thumb => '400x250#',
+                        :small => '640>',
+                        :medium => '1024>',
+                        :large => '1200>',
+                        :xlarge => '1920>',
+                    },
+                    :convert_options => {
+                        :thumb => "-quality 75 -strip",
+                        :small => "-quality 75 -strip",
+                        :medium => "-quality 75 -strip",
+                        :large => "-quality 75 -strip",
+                        :xlarge => "-quality 75 -strip"
+                    },
                     :default_url => "img/missing.png"
 
-  validates_attachment_size :immagine, :less_than => 3.megabytes
+  validates_attachment_size :immagine, :less_than => 8.megabytes
   validates_attachment_content_type :immagine, :content_type => /\Aimage/
 end

a715c62a1c6005af0f0d31b43c3e14acb0645076
Andrea Ros <andrea@donboscoland.it>
2017-01-18 18:56:38 +0100

    aggiunto autocrop alle colonne della row0

--- app/helpers/columns_helper.rb
+++ app/helpers/columns_helper.rb
@@ -1,2 +1,9 @@
 module ColumnsHelper
+  def cropped_image image, percentage, autocrop
+    if autocrop
+      "<div class='cropped-#{percentage}'><img src='#{image}' class='cropped-image'></div>".html_safe
+    else
+      "<img src='#{image}' style='width: 100%;'>".html_safe
+    end
+  end
 end

2e648cc766fe53f6c943b94ee26e2958cbc03c4f
Andrea Ros <andrea@donboscoland.it>
2017-07-07 21:51:36 +0200

    test con capybara

--- test/features/pages_test.rb
+++ test/features/pages_test.rb
@@ -1,14 +1,12 @@
 require "test_helper"
 
 class PagesTest < Capybara::Rails::TestCase
-  # todo: provare creazione di una nuova pagina
-
-
   test "l'amministratore gestisce le pagine" do
     logon_as_administrator
 
     visit '/pages'
 
+    # verifica dell'esistenza di alcuni elementi
     assert page.has_link?("NUOVA PAGINA")
     assert page.has_text?("/home-sezione-1")
     assert page.has_text?("/footer")
@@ -16,6 +14,28 @@ class PagesTest < Capybara::Rails::TestCase
     assert page.has_text?("/home")
     assert page.has_text?("/modello")
 
+    # creazione di una nuova pagina
+    click_on("NUOVA PAGINA")
+
+    fill_in 'page_titolo', :with => "Pagina nuova"
+    select "Principale", :from => 'page_section_id'
+    click_on "SALVA"
+
+    assert page.has_text?("Cantami o Diva del pelide Achille")
+
+    visit '/pages'
+    assert page.has_text?("/pagina-nuova")
+
+    # modifica delle proprietà di una pagina
+    page.find_link('Proprietà', :href => '/pages/pagina-nuova/edit').click
+    fill_in 'page_titolo', :with => "Pagina nuova 2"
+    check 'rigenera_slug'
+    click_on "SALVA"
+
+    assert page.has_text?("/pagina-nuova-2")
+
+    # duplicazione di una pagina
+    visit '/pages'
     page.find_link('Duplica', :href => '/pages/home-sezione-1/duplica').click
 
     fill_in 'page_titolo', :with => "Pagina modificata"
@@ -25,6 +45,7 @@ class PagesTest < Capybara::Rails::TestCase
 
     assert page.has_text?("/pagina-modificata")
 
+    # eliminazione di una pagina
     page.find_link('Elimina', :href => '/pages/pagina-modificata?section_id=1').click
 
     assert page.has_no_text?("/pagina-modificata")
@@ -35,12 +56,34 @@ class PagesTest < Capybara::Rails::TestCase
 
     visit '/pages'
 
+    # verifica dell'esistenza di alcuni elementi
     assert page.has_link?("NUOVA PAGINA")
     assert page.has_text?("/home-sezione-1")
     assert page.has_no_text?("/footer")
     assert page.has_no_text?("/header")
     assert page.has_no_text?("/modello")
 
+    # creazione di una nuova pagina
+    click_on("NUOVA PAGINA")
+
+    fill_in 'page_titolo', :with => "Pagina nuova"
+    click_on "SALVA"
+
+    assert page.has_text?("Cantami o Diva del pelide Achille")
+
+    visit '/pages'
+    assert page.has_text?("/pagina-nuova")
+
+    # modifica delle proprietà di una pagina
+    page.find_link('Proprietà', :href => '/pages/pagina-nuova/edit').click
+    fill_in 'page_titolo', :with => "Pagina nuova 2"
+    check 'rigenera_slug'
+    click_on "SALVA"
+
+    assert page.has_text?("/pagina-nuova-2")
+
+    # duplicazione di una pagina
+    visit '/pages'
     page.find_link('Duplica', :href => '/pages/home-sezione-1/duplica').click
 
     fill_in 'page_titolo', :with => "Pagina modificata"
@@ -49,6 +92,7 @@ class PagesTest < Capybara::Rails::TestCase
 
     assert page.has_text?("/pagina-modificata")
 
+    # eliminazione di una pagina
     page.find_link('Elimina', :href => '/pages/pagina-modificata?section_id=6').click
 
     assert page.has_no_text?("/pagina-modificata")
@@ -59,12 +103,34 @@ class PagesTest < Capybara::Rails::TestCase
 
     visit '/pages'
 
+    # verifica dell'esistenza di alcuni elementi
     assert page.has_link?("NUOVA PAGINA")
     assert page.has_text?("/home-sezione-1")
     assert page.has_no_text?("/footer")
     assert page.has_no_text?("/header")
     assert page.has_no_text?("/modello")
 
+    # creazione di una nuova pagina
+    click_on("NUOVA PAGINA")
+
+    fill_in 'page_titolo', :with => "Pagina nuova"
+    click_on "SALVA"
+
+    assert page.has_text?("Cantami o Diva del pelide Achille")
+
+    visit '/pages'
+    assert page.has_text?("/pagina-nuova")
+
+    # modifica delle proprietà di una pagina
+    page.find_link('Proprietà', :href => '/pages/pagina-nuova/edit').click
+    fill_in 'page_titolo', :with => "Pagina nuova 2"
+    check 'rigenera_slug'
+    click_on "SALVA"
+
+    assert page.has_text?("/pagina-nuova-2")
+
+    # duplicazione di una pagina
+    visit '/pages'
     page.find_link('Duplica', :href => '/pages/home-sezione-1/duplica').click
 
     fill_in 'page_titolo', :with => "Pagina modificata"
@@ -73,6 +139,7 @@ class PagesTest < Capybara::Rails::TestCase
 
     assert page.has_text?("/pagina-modificata")
 
+    # eliminazione di una pagina
     page.find_link('Elimina', :href => '/pages/pagina-modificata?section_id=6').click
 
     assert page.has_no_text?("/pagina-modificata")

359e2c912767d3ae1befadb0233824f6c270e6f8
Andrea Ros <andrea@donboscoland.it>
2017-08-21 21:26:36 +0200

    esportazione instances su excel

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -44,7 +44,7 @@ class AssignmentsController < ApplicationController
 
     respond_to do |format|
       if @assignment.save
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
+        format.html { redirect_to assignments_url }
         format.json { render :show, status: :created, location: @assignment }
       else
         format.html { render :new }
@@ -58,7 +58,7 @@ class AssignmentsController < ApplicationController
   def update
     respond_to do |format|
       if @assignment.update(assignment_params)
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully updated.' }
+        format.html { redirect_to assignments_url }
         format.json { render :show, status: :ok, location: @assignment }
       else
         format.html { render :edit }
@@ -72,7 +72,7 @@ class AssignmentsController < ApplicationController
   def destroy
     @assignment.destroy
     respond_to do |format|
-      format.html { redirect_to assignments_url, notice: 'Assignment was successfully destroyed.' }
+      format.html { redirect_to assignments_url }
       format.json { head :no_content }
     end
   end

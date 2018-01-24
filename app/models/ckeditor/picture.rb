3844a7990e6e925eec6df7579ae94d4719e93ce0
Andrea Ros <andrea@donboscoland.it>
2018-01-24 17:26:39 +0100

    permesso html su help text delle properties

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -15,7 +15,7 @@ class Ckeditor::Picture < Ckeditor::Asset
                     :default_url => "img/missing.png"
 
   validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 2.megabytes
+  validates_attachment_size :data, :less_than => 4.megabytes
   validates_attachment_content_type :data, :content_type => /\Aimage/
 
   def url_content
